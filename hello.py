b089f86163cc7b9a48876fb7adbed5be514162e1
Ziyu Wang <ziyu.wang@students.olin.edu>
2015-10-17 15:23:12 -0400

    hello.py: Adds a new file hello.py
    Added a function that takes an input and print out twice that value.
    Deleted unnecessary comments. Fixed typo in doc string

---
+++ hello.py
@@ -0,0 +1,16 @@
+"""
+Multiply input by two and print the result
+
+"""
+
+def multiply_by_two(x):
+    """ Multiplies the input value by 2 and prints the result
+
+        x: the value to multiply by 2
+    """
+
+    print 2*x
+
+multiply_by_two(3);
+
+
